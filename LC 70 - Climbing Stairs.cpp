7052ff53a1550aa96f607e05cd9e2f6231979174
Mayank Srivastava <89370058+Mayank2553@users.noreply.github.com>
2025-02-13 21:07:10 +0530

    Create LC 70 - Climbing Stairs.cpp

---
+++ LC 70 - Climbing Stairs.cpp
@@ -0,0 +1,34 @@
+// https://leetcode.com/problems/climbing-stairs/
+
+class Solution {
+public:
+
+    int climbHelper(int n, vector<int>& memo){
+        if(n==1) return 1;
+        if(n==2) return 2;
+
+        if(memo[n] != -1) return memo[n];
+
+        memo[n] = climbHelper(n-1, memo) + climbHelper(n-2, memo);
+        return memo[n];
+    }
+
+    int climbStairs(int n) {
+        // if(n==0){
+        //     return 0;
+        // }
+        // if(n==1){
+        //     return 1;
+        // }
+        // int ans = 0, s1 = 0, s2 = 1;
+        // for(int i=1; i<=n; i++){
+        //     ans = s1+s2;
+        //     s1 = s2;
+        //     s2 = ans;
+        // }
+        // return ans;
+
+        vector<int> memo(n+1, -1);
+        return climbHelper(n, memo);
+    }
+};

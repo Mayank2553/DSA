53cc6e7935465d8835dd6144fb5579041db70011
Mayank Srivastava <89370058+Mayank2553@users.noreply.github.com>
2025-02-15 11:34:00 +0530

    Time: 48 ms (59.75%) | Memory: 45.5 MB (56.88%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -2,26 +2,30 @@ class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
         //Brute Force Approach
-        int count = 0;
-        for(int i=0; i<nums.size(); i++){
-            int sum = 0;
-            for(int j = i; j<nums.size(); j++){
-                sum += nums[j];
-                if(sum == k){
-                    count++;
-                }
+        // int count = 0;
+        // for(int i=0; i<nums.size(); i++){
+        //     int sum = 0;
+        //     for(int j = i; j<nums.size(); j++){
+        //         sum += nums[j];
+        //         if(sum == k){
+        //             count++;
+        //         }
+        //     }
+        // }
+        // return count;
+        
+        
+        unordered_map<int, int> prefixSum;
+        prefixSum[0] = 1;
+        int sum = 0, count = 0;
+        for(int num: nums){
+            sum += num;
+
+            if(prefixSum.find(sum - k) != prefixSum.end()){
+                count += prefixSum[sum-k];
             }
+            prefixSum[sum]++;
         }
         return count;
-        
-        
-        // unordered_map<int, int> m;
-        // subNum[0] = 1;
-        // int total = 0, count = 0;
-        // for(int i: nums){
-        //     total += n;
-
-        //     if(m)
-        // }
     }
 };

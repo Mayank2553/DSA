a46e1a3c3a1ab56fa108dbcc98b299b55256efc7
Mayank Srivastava <89370058+Mayank2553@users.noreply.github.com>
2025-02-16 10:18:33 +0530

    Time: 0 ms (100.00%) | Memory: 23.9 MB (50.06%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -2,19 +2,30 @@ class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
         int n = nums.size();
-        int nz = 0;
-        vector<int> temp;
+
+        //Brute Force - using a temporary array to store non zeroes
+        // int nz = 0;
+        // vector<int> temp;
+        // for(int i=0; i<n; i++){
+        //     if(nums[i] != 0){
+        //         temp.push_back(nums[i]);
+        //         nz++;
+        //     }
+        // }
+        // for(int i=0; i<temp.size(); i++){
+        //     nums[i] = temp[i];
+        // }
+        // for(int i = nz; i<n; i++){
+        //     nums[i] = 0;
+        // }
+
+        //Optimal - Two Pointers Approach
+        int j = 0;
         for(int i=0; i<n; i++){
             if(nums[i] != 0){
-                temp.push_back(nums[i]);
-                nz++;
+                swap(nums[i], nums[j]);
+                j++;
             }
-        }
-        for(int i=0; i<temp.size(); i++){
-            nums[i] = temp[i];
-        }
-        for(int i = nz; i<n; i++){
-            nums[i] = 0;
         }
     }
 };

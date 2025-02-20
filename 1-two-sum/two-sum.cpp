b04c5dfdff1537b72fcbc38a534571c8ac0afedb
Mayank Srivastava <89370058+Mayank2553@users.noreply.github.com>
2025-02-20 23:23:43 +0530

    Time: 3 ms (74.05%) | Memory: 15 MB (15.12%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -2,16 +2,30 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         //1. Brute Force
-        vector<int> ans;
+        // vector<int> ans;
+        // int n = nums.size();
+        // for(int i=0; i<n; i++){
+        //     for(int j=i+1; j<n; j++){
+        //         if(nums[i] + nums[j] == target){
+        //             ans.push_back(i);
+        //             ans.push_back(j);
+        //         }
+        //     }
+        // }
+        // return ans;
+
+        //Better Solution
         int n = nums.size();
+        map<int, int> m;
+        vector<int> ans;
+
         for(int i=0; i<n; i++){
-            for(int j=i+1; j<n; j++){
-                if(nums[i] + nums[j] == target){
-                    ans.push_back(i);
-                    ans.push_back(j);
-                }
+            int comp = target - nums[i];
+            if(m.count(comp)){
+                return {m[comp], i};
             }
+            m[nums[i]] = i;
         }
-        return ans;
+        return {};
     }
 };

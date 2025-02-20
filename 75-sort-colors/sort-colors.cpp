af6fb0ee66f77fce3a66f9cfdc2d0de7e7ec061a
Mayank Srivastava <89370058+Mayank2553@users.noreply.github.com>
2025-02-21 00:31:21 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (31.23%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,23 +1,41 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int c0 = 0, c1 = 0, c2 = 0;
-        for(int i=0; i<nums.size(); i++){
-            if(nums[i]==0) c0++;
-            else if(nums[i]==1) c1++;
-            else c2++;
-        }
+        //Better Approach
+        // int c0 = 0, c1 = 0, c2 = 0;
+        // for(int i=0; i<nums.size(); i++){
+        //     if(nums[i]==0) c0++;
+        //     else if(nums[i]==1) c1++;
+        //     else c2++;
+        // }
 
-        int idx = 0;
+        // int idx = 0;
 
-        for(int i=0; i<c0; i++){
-            nums[idx++] = 0;
-        }
-        for(int i=0; i<c1; i++){
-            nums[idx++] = 1;
-        }
-        for(int i=0; i<c2; i++){
-            nums[idx++] = 2;
+        // for(int i=0; i<c0; i++){
+        //     nums[idx++] = 0;
+        // }
+        // for(int i=0; i<c1; i++){
+        //     nums[idx++] = 1;
+        // }
+        // for(int i=0; i<c2; i++){
+        //     nums[idx++] = 2;
+        // }
+
+        //Optimal Approach - Dutch National Flag Algo
+        int low = 0, mid = 0, high = nums.size()-1;
+        while(mid<=high){
+            if(nums[mid] == 0){
+                swap(nums[low], nums[mid]);
+                low++;
+                mid++;
+            }
+            else if(nums[mid] == 1){
+                mid++;
+            }
+            else{
+                swap(nums[mid], nums[high]);
+                high--;
+            }
         }
     }
 };

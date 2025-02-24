8c6047b3b93a15e7a19c0b3851e572b48e9be21a
Mayank Srivastava <89370058+Mayank2553@users.noreply.github.com>
2025-02-24 20:44:58 +0530

    Time: 3 ms (30.56%) | Memory: 28.3 MB (24.50%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,8 +1,29 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
+
+        //3. Optimal Approach - Moore's Voting Algo
+        int n = nums.size();
+        int count = 0;
+        int el;
+        for(int i=0; i<n; i++){
+            if(count == 0){
+                count = 1;
+                el = nums[i];
+            }
+            else if(nums[i] == el) count++;
+            else count--;
+        }
+
+        int cnt1=0;
+        for(int i=0; i<n; i++){
+            if(nums[i] == el) cnt1++;
+        }
+
+        if(cnt1 > (n/2)) return el;
+        return -1;
         
-        //1. Brute Force Approach
+        // 1. Brute Force Approach
         // int n = nums.size();
         // for(int i=0; i<n; i++){
         //     int count = 0;
@@ -17,17 +38,21 @@ public:
         // }
         // return -1;
 
-        //2. Better Approach - O(NlogN)
-        int n = nums.size();
-        map<int, int> mp;
-        for(auto it: nums){
-            mp[it]++;
-        }
-        for(auto it: mp){
-            if(it.second > (n/2)){
-                return it.first;
-            }
-        }
-        return -1;
+        //2. Better Approach 
+        // int n = nums.size();
+        // map<int, int> mp;
+        // for(auto it: nums){
+        //     mp[it]++;
+        // }
+        // for(auto it: mp){
+        //     if(it.second > (n/2)){
+        //         return it.first;
+        //     }
+        // }
+        // return -1;
+        // Tc - O(N), SC - O(N)
+
+        //3. Optimal Approach
+
     }
 };

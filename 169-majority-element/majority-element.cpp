104b4ff8f600a7dc1fc466d79c37933a3798c966
Mayank Srivastava <89370058+Mayank2553@users.noreply.github.com>
2025-02-24 18:52:49 +0530

    Time: 0 ms (100.00%) | Memory: 28.1 MB (58.63%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,16 +1,31 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
+        
+        //1. Brute Force Approach
+        // int n = nums.size();
+        // for(int i=0; i<n; i++){
+        //     int count = 0;
+        //     for(int j=0; j<n; j++){
+        //         if(nums[i] == nums[j]){
+        //             count++;
+        //         }
+        //     }
+        //     if(count>n/2){
+        //         return nums[i];
+        //     }
+        // }
+        // return -1;
+
+        //2. Better Approach - O(NlogN)
         int n = nums.size();
-        for(int i=0; i<n; i++){
-            int count = 0;
-            for(int j=0; j<n; j++){
-                if(nums[i] == nums[j]){
-                    count++;
-                }
-            }
-            if(count>n/2){
-                return nums[i];
+        map<int, int> mp;
+        for(auto it: nums){
+            mp[it]++;
+        }
+        for(auto it: mp){
+            if(it.second > (n/2)){
+                return it.first;
             }
         }
         return -1;

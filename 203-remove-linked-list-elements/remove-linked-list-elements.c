3f09b8436e1e6e04ed867a83b3c0892fad1c8cf7
Mayank Srivastava <89370058+Mayank2553@users.noreply.github.com>
2025-02-25 15:53:21 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (80.91%) - LeetSync

--- 203-remove-linked-list-elements/remove-linked-list-elements.c
+++ 203-remove-linked-list-elements/remove-linked-list-elements.c
@@ -9,9 +9,13 @@ struct ListNode* removeElements(struct ListNode* head, int val) {
     struct ListNode *temp = (struct ListNode*)malloc(sizeof(struct ListNode));
     temp->next = head;
     struct ListNode *curr = temp;
-    while(curr->next != NULL ){
-        if(curr->next->val == val) curr->next = curr->next->next;
-        else curr = curr->next;
+    while(curr->next != NULL){
+        if(curr->next->val == val){
+            curr->next = curr->next->next;
+        } 
+        else{
+            curr = curr->next;
+        } 
     }
     return temp->next;
 }

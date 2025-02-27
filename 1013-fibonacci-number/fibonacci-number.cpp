6cd82d655b95ef706a06b42a848bdd648e4afd4c
Mayank Srivastava <89370058+Mayank2553@users.noreply.github.com>
2025-02-27 11:04:18 +0530

    Time: 2 ms (79.04%) | Memory: 7.9 MB (34.42%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,14 +1,18 @@
 class Solution {
 public:
     int fib(int n) {
-        int ans = 0;
-        if(n==0){
-            return 0;
+        if(n==0 || n==1) return n;
+        int arr[n+1];
+        arr[0] = 0, arr[1] = 1;
+        for(int i=2; i<n+1; i++){
+            arr[i] = -1;
         }
-        else if( n==1){
-            return 1;
+        return helper(arr, n);
+    }
+    int helper(int arr[], int n){
+        if(arr[n] != -1) return arr[n];
+        else{
+            return arr[n] = helper(arr, n-1) + helper(arr, n-2);
         }
-        ans = fib(n-1) + fib(n-2);
-        return ans;
     }
 };

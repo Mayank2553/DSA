980436fda04efbfdb24425ba5ffe2714d3481358
Mayank Srivastava <89370058+Mayank2553@users.noreply.github.com>
2025-02-27 12:23:38 +0530

    Time: 0 ms (100.00%) | Memory: 11.2 MB (5.51%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.cpp
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -16,11 +16,14 @@ public:
         vector<int> prev = ans[n-2];
         vector<int> curr;
         curr.push_back(1);
+
         for(int i=1; i<n-1; i++){
             curr.push_back(prev[i]+prev[i-1]);
         }
+
         curr.push_back(1);
         ans.push_back(curr);
         return ans;
+
     }
 };

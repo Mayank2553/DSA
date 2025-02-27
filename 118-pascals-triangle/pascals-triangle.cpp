47eb4c737a5a6b04a8a4d80a28667be3c8090c5d
Mayank Srivastava <89370058+Mayank2553@users.noreply.github.com>
2025-02-27 12:14:38 +0530

    Time: 1 ms (20.12%) | Memory: 11.4 MB (5.51%) - LeetSync

---
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int n) {
+        vector<vector<int>> ans;
+        if(n==0){
+            return ans;
+        }
+        if(n==1){
+            vector<int> temp;
+            temp.push_back(1);
+            ans.push_back(temp);
+            return ans;
+        }
+
+        ans = generate(n-1);
+        vector<int> prev = ans[n-2];
+        vector<int> curr;
+        curr.push_back(1);
+        for(int i=1; i<n-1; i++){
+            curr.push_back(prev[i]+prev[i-1]);
+        }
+        curr.push_back(1);
+        ans.push_back(curr);
+        return ans;
+    }
+};

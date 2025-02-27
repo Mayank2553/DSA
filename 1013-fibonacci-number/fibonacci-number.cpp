2165c14d3252f86b9d4e323a4dca082f986cefd0
Mayank Srivastava <89370058+Mayank2553@users.noreply.github.com>
2025-02-27 11:14:16 +0530

    Time: 3 ms (68.01%) | Memory: 7.9 MB (18.52%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,18 +1,34 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n==0 || n==1) return n;
-        int arr[n+1];
-        arr[0] = 0, arr[1] = 1;
+        //     if(n==0 || n==1) return n;
+        //     int arr[n+1];
+        //     arr[0] = 0, arr[1] = 1;
+        //     for(int i=2; i<n+1; i++){
+        //         arr[i] = -1;
+        //     }
+        //     return helper(arr, n);
+        // }
+        // int helper(int arr[], int n){
+        //     if(arr[n] != -1) return arr[n];
+        //     else{
+        //         return arr[n] = helper(arr, n-1) + helper(arr, n-2);
+        //     }
+        // }
+        if(n==0){
+            return 0;
+        }
+        if(n==1){
+            return 1;
+        }
+        int f1 = 0, f2 = 1;
+        int fib3, ans;
         for(int i=2; i<n+1; i++){
-            arr[i] = -1;
+            fib3 = f2+f1;
+            int temp = f2;
+            f2 = fib3;
+            f1 = temp;
         }
-        return helper(arr, n);
-    }
-    int helper(int arr[], int n){
-        if(arr[n] != -1) return arr[n];
-        else{
-            return arr[n] = helper(arr, n-1) + helper(arr, n-2);
-        }
+        return fib3;
     }
 };

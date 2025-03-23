970a30034163b5045c9a540425de36a91631a2a4
Mayank Srivastava <89370058+Mayank2553@users.noreply.github.com>
2025-03-23 22:34:52 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (89.17%) - LeetSync

--- 908-middle-of-the-linked-list/middle-of-the-linked-list.cpp
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.cpp
@@ -11,21 +11,31 @@
 class Solution {
 public:
     ListNode* middleNode(ListNode* head) {
-        ListNode* temp = head;
-        int count = 0;
-        while(temp){
-            count++;
-            temp = temp->next;
+        //1. Brute Force approach
+        // ListNode* temp = head;
+        // int count = 0;
+        // while(temp){
+        //     count++;
+        //     temp = temp->next;
+        // }
+        // int midNode = (count/2) + 1;
+        // temp = head;
+        // while(temp){
+        //     midNode -= 1;
+        //     if(midNode == 0){
+        //         break;
+        //     }
+        //     temp = temp->next;
+        // }
+        // return temp;
+
+
+        //2. Tortoise and Hare Method
+        ListNode* slow = head, *fast = head;
+        while(fast != NULL && fast->next != NULL){
+            slow = slow->next;
+            fast = fast->next->next;
         }
-        int midNode = (count/2) + 1;
-        temp = head;
-        while(temp){
-            midNode -= 1;
-            if(midNode == 0){
-                break;
-            }
-            temp = temp->next;
-        }
-        return temp;
+        return slow;
     }
 };

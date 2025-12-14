b145cef262f1d00f64414fb70fccc35bdff0c1c6
Mayank Srivastava <89370058+Mayank2553@users.noreply.github.com>
2025-12-14 20:01:53 +0530

    Time: 16 ms (59.73%) | Memory: 10.7 MB (28.80%) - LeetSync

--- 1351-replace-the-substring-for-balanced-string/replace-the-substring-for-balanced-string.cpp
+++ 1351-replace-the-substring-for-balanced-string/replace-the-substring-for-balanced-string.cpp
@@ -2,36 +2,22 @@ class Solution {
 public:
     int balancedString(string s) {
         int n = s.size();
-        int need = n / 4;
-        vector<int> freq(128, 0);
+        int need = n/4;
 
-        // 1) full string frequency
-        for (char c : s) freq[(int)c]++;
+        unordered_map<char, int> map;
+        for(char c: s) map[c]++;
 
-        // If already balanced (all <= need), answer is 0
-        if (freq['Q'] <= need && freq['W'] <= need && freq['E'] <= need && freq['R'] <= need)
-            return 0;
+        int ans = n, l = 0;
+        for(int r=0; r<n; r++){
+            map[s[r]]--; //include s[r] in window
 
-        int ans = n;        // worst-case: replace whole string
-        int L = 0;
-
-        for (int R = 0; R < n; ++R) {
-            // include s[R] in the window => remove it from "outside" counts
-            freq[(int)s[R]]--;
-
-            // While outside is valid (all chars <= need), try to shrink from left
-            while (L <= R &&
-                   freq['Q'] <= need &&
-                   freq['W'] <= need &&
-                   freq['E'] <= need &&
-                   freq['R'] <= need) {
-                ans = min(ans, R - L + 1);     // current window is valid
-                // move left edge right: put s[L] back into outside
-                freq[(int)s[L]]++;
-                ++L;
+            while(l < n && map['Q'] <= need && map['W'] <= need 
+            && map['E'] <= need && map['R'] <= need) {
+                ans = min(r-l+1, ans);
+                map[s[l]]++; //remove s[l] from window
+                l++;
             }
         }
-
         return ans;
     }
 };

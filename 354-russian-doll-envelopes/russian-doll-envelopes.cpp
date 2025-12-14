8bf23ffd443cbab5d71474fb7e0a076443f98332
Mayank Srivastava <89370058+Mayank2553@users.noreply.github.com>
2025-12-14 19:25:57 +0530

    Time: 37 ms (78.04%) | Memory: 81.6 MB (75.19%) - LeetSync

--- 354-russian-doll-envelopes/russian-doll-envelopes.cpp
+++ 354-russian-doll-envelopes/russian-doll-envelopes.cpp
@@ -1,34 +1,17 @@
 class Solution {
 public:
-    int lengthOfLIS(vector<int>& nums) {
-        vector<int> lis;
-        for (int i = 0; i < nums.size(); i++) {
-            auto it = lower_bound(lis.begin(), lis.end(), nums[i]);
-            if (it == lis.end()) {
-                lis.push_back(nums[i]);
-            } else {
-                *it = nums[i];
-            }
-        }
-        return lis.size();
-    }
-
     int maxEnvelopes(vector<vector<int>>& env) {
-        sort(env.begin(), env.end(), [](auto& a, auto& b) {
-            if (a[0] == b[0])
-                return a[1] > b[1];
-            return a[0] < b[0];
+        sort(env.begin(), env.end(), [](auto &a, auto &b){
+            if(a[0] != b[0]) return a[0] < b[0];
+            return a[1] > b[1];
         });
 
         vector<int> lis;
-        for (auto& e : env) {
+        for(auto &e: env){
             int h = e[1];
             auto it = lower_bound(lis.begin(), lis.end(), h);
-            if (it == lis.end()) {
-                lis.push_back(h);
-            } else {
-                *it = h;
-            }
+            if(it == lis.end()) lis.push_back(h);
+            else *it = h;
         }
         return lis.size();
     }

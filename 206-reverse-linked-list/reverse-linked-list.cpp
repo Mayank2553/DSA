689ff4de4dac1d731ee0aa16be5edc8be07f85da
Mayank Srivastava <89370058+Mayank2553@users.noreply.github.com>
2026-02-18 19:39:37 +0530

    Time: 0 ms (100.00%) | Memory: 13.5 MB (42.41%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.cpp
+++ 206-reverse-linked-list/reverse-linked-list.cpp
@@ -11,42 +11,14 @@
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
-        //0. Brute Force - Vector based node value changing
-        // vector<int> ans;
-        // ListNode * temp = head;
-        // while(temp){
-        //     ans.push_back(temp->val);
-        //     temp = temp->next;
-        // }
-        // // reverse(ans.begin(), ans.end());
-        // int i=ans.size()-1;
-        // temp = head;
-        // while(temp){
-        //     temp->val = ans[i];
-        //     temp = temp->next;
-        //     i--;
-        // }
-        // return head;
-
-        //1. Iterative - changing pointers
-        // ListNode* curr = head, *prev = NULL, *future = NULL;
-        // while(curr){
-        //     future = curr->next;
-        //     curr->next = prev;
-        //     prev = curr;
-        //     curr = future;
-        // }
-        // head = prev;
-        // return head;
-
-        //2. Recursive
-        if(head == NULL || head->next == NULL){
-            return head;
+        ListNode* curr = head;
+        ListNode* prev = NULL;
+        while(curr != NULL){
+            ListNode* temp = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = temp;
         }
-        ListNode* newHead = reverseList(head->next);
-        ListNode* front = head->next;
-        front->next = head;
-        head->next = NULL;
-        return newHead;
+        return prev;
     }
 };

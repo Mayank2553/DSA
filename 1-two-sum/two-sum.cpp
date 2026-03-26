d9e64177ab8a943974396659b568b00846e35174
Mayank Srivastava <89370058+Mayank2553@users.noreply.github.com>
2026-03-26 19:48:34 +0530

    Time: 35 ms (36.76%) | Memory: 14.1 MB (66.68%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,29 +1,28 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> mpp;
-        for(int i=0; i<nums.size(); i++){
-            mpp[nums[i]] = i;
-        }
-        for(int i=0; i<nums.size(); i++){
-            int complement = target - nums[i];
-            if(mpp.find(complement) != mpp.end() && mpp[complement] != i){
-                return {i, mpp[complement]};
+        // unordered_map<int, int> mpp;
+        // for(int i=0; i<nums.size(); i++){
+        //     mpp[nums[i]] = i;
+        // }
+        // for(int i=0; i<nums.size(); i++){
+        //     int complement = target - nums[i];
+        //     if(mpp.find(complement) != mpp.end() && mpp[complement] != i){
+        //         return {i, mpp[complement]};
+        //     }
+        // }
+        // return {};
+        
+        //brute force
+        int n = nums.size();
+        for(int i=0; i<n-1; i++){
+            for(int j=i+1; j<n; j++){
+                if(nums[i] + nums[j] == target){
+                    return {i, j};
+                }
             }
         }
         return {};
-        //brute force
-        // int n = nums.size();
-        // vector<int> result;
-        // for(int i=0; i<n-1; i++){
-        //     for(int j=i+1; j<n; j++){
-        //         if(nums[i] + nums[j] == target){
-        //             result.push_back(i);
-        //             result.push_back(j);
-        //         }
-        //     }
-        // }
-        // return result;
 
     }
 };

79fa42ff6a5a55bfdbad6e84f48a8dbdb6dabff7
Mayank Srivastava <89370058+Mayank2553@users.noreply.github.com>
2026-03-27 17:26:30 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (91.29%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -2,19 +2,36 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
         if(s.size() != t.size()) return false;
-        unordered_map<char, int> mpp;
-        for(int i=0; i<s.size(); i++){
-            mpp[s[i]]++;
+        
+        vector<int> count(26, 0);
+
+        for (char c : s) {
+            count[c - 'a'] += 1;
         }
-        for(int i=0; i<t.size(); i++){
-            // if(mpp.find(t[i]) == mpp.end()){
-            //     return false;
-            // }
-            mpp[t[i]]--;
-            if(mpp[t[i]] < 0){
+
+        for (char c : t) {
+            if (count[c - 'a'] == 0) {
                 return false;
             }
+            count[c - 'a'] -= 1;
         }
-        return true;
+
+        return true;  
+        
+        
+        // unordered_map<char, int> mpp;
+        // for(int i=0; i<s.size(); i++){
+        //     mpp[s[i]]++;
+        // }
+        // for(int i=0; i<t.size(); i++){
+        //     // if(mpp.find(t[i]) == mpp.end()){
+        //     //     return false;
+        //     // }
+        //     mpp[t[i]]--;
+        //     if(mpp[t[i]] < 0){
+        //         return false;
+        //     }
+        // }
+        // return true;
     }
 };
